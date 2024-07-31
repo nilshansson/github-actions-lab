795df69544c1e87b6c404808c31882f8b7524172
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-31 11:07:48 +0200

    drizzle added to paymentapp
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

---
+++ drizzle.ts
@@ -0,0 +1,23 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {
+  numeric,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+import { uuid } from "drizzle-orm/pg-core";
+import { jsonb } from "drizzle-orm/pg-core";
+export const db = drizzle(sql);
+export const paymentTable = pgTable("payment", {
+  id: serial("id").primaryKey(),
+  carId: uuid("carId").notNull(),
+  amount: numeric("amount").notNull(),
+});
+
+export const outboxTable = pgTable("outbox", {
+  id: serial("id").primaryKey(),
+  data: jsonb("data").notNull(),
+});
